007a8266647021289468b5cca16155d2c25b17b7
HsrRaza <hr.raza04@gmail.com>
2026-02-28 23:33:55 +0530

    rev/streak

--- revision.js
+++ revision.js
@@ -50,3 +50,19 @@ console.log(countsDigits(872));
 
 
 
+// Write a function that returns the largest number in an array.
+
+
+function largestNuminArr(...num){
+    let largest = - Infinity;
+    
+    for(let i = 0; i < num.length; i++){
+        if(num[i] > largest ){
+            largest = num[i]
+        }
+    }
+
+    return largest
+}
+
+console.log(largestNuminArr(2, -6, 4, 8, 1, -9));
